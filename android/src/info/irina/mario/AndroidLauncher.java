65bc496d5d325106555472bf096a0150504e1747
Irina Chernykh <chernykh.irina.1996@gmail.com>
2016-08-28 20:10:17 +0300

    map

---
+++ android/src/info/irina/mario/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package info.irina.mario;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import info.irina.mario.Mario;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new Mario(), config);
+	}
+}
---
+++ desktop/src/info/irina/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,23 @@
+package info.irina.mario.desktop;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.irina.mario.Mario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Mario(), config);
+	}
+}
+
+/*public class Game implements ApplicationListener {
+
+	public static final String TITLE = "Mario";
+	public static final int V_WIDTH =
+
+}*/
